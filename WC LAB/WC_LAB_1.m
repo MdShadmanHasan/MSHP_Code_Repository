968358548f9b6f3c6b38bd57176ed649d03fe01b
MdShadmanHasan <shpartho@gmail.com>
2017-04-17 13:19:48 +0600

    Updated WC Lab file

--- WC LAB/WC_LAB_1.m
+++ WC LAB/WC_LAB_1.m
@@ -14,3 +14,19 @@ if (remainder == 0)
 else
     UserPaddedData = [InputReshapeBit, zeros(1, bps-remainder)];
 end
+
+ThreePaddedData = reshape(UserPaddedData, [], bps);
+ModulationInput = bi2de(ThreePaddedData);
+PSKModulatedSymbol = pskmod(ModulationInput, M);
+
+%for snr = 1:3:25
+%SignalThroughTheChannel = awgn(PSKModulatedSymbol, snr, 'measured');
+%end
+
+SignalThroughTheChannel = awgn(PSKModulatedSymbol, 25, 'measured');
+
+DemodulatedSignal = pskdemod(SignalThroughTheChannel, M);
+SymbolToBitMappingOfDemodulatedSignal = de2bi(DemodulatedSignal);
+DemodPaddedData = reshape(SymbolToBitMappingOfDemodulatedSignal, bps, []);
+
+%PaddingRemovedData = 
