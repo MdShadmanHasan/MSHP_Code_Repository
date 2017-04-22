d3c4897c569bc7b15c3c3d279591a75238f5fd12
MdShadmanHasan <shpartho@gmail.com>
2017-04-22 13:27:04 +0600

    DBMS Discussion_2 Day_2

---
+++ Projects/DBMS_discussion_2_day_2.sql
@@ -0,0 +1,8 @@
+SELECT * FROM authors
+SELECT * FROM titleauthor
+SELECT * FROM titles
+SELECT * FROM publishers
+
+SELECT au_lname, title_id FROM authors JOIN titleauthor ON authors.au_id=titleauthor.au_id
+SELECT title, au_fname+ ' ' +au_lname as author_name FROM titles JOIN titleauthor ON titles.title_id=titleauthor.title_id JOIN authors ON authors.au_id=titleauthor.au_id
+SELECT title, au_fname+ ' ' +au_lname as author_name, pub_name FROM titles JOIN titleauthor ON titles.title_id=titleauthor.title_id JOIN authors ON authors.au_id=titleauthor.au_id JOIN publishers ON titles.pub_id=publishers.pub_id
